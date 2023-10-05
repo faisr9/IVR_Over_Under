7c3e796c3ad2254f55107f126782a81616f29245
ddai20 <ddai20@illinois.edu>
2023-10-04 19:48:43 -0500

    skills_15_auton

---
+++ include/skills_15/auton.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "api.h"
+#include <vector>
+
+void drivePID(double inches);
+void shootPID(double angle);
+void rollerAuto();
+void boltSkillsAuto();
+void boltEndgameAuto();
+void StraightPathTest();
---
+++ src/skills_15/auton.cpp
@@ -0,0 +1,36 @@
+#include "main.h"
+#include "pros/llemu.hpp"
+#include "pros/misc.h"
+#include "pros/rtos.hpp"
+#include <cmath>
+#include <ctime>
+#include <string>
+#include <vector>
+
+void drivePID(double inches) {
+
+}
+
+void shootPID(double angle) {
+
+}
+
+void rollerAuto() {
+
+}
+
+void getOneFromStack() {
+
+}
+
+void boltSkillsAuto() {
+
+}
+
+void boltEndgameAuto() {
+
+}
+
+void StraightPathTest() {
+
+}
