4682690fc0df0156c75df2ab4394fb5276d8d279
Bananapad987 <Bananapad987@users.noreply.github.com>
2023-11-01 18:30:29 -0500

    Wrote a bit more in x_drive class

---
+++ include/common_code/x_drive.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "api.h"
+#include <cmath>
+using namespace pros;
+class x_drive{
+    public:
+        x_drive(Controller master, Motor front_left, Motor front_right, Motor back_left, Motor back_right, Imu imu);
+        void robot_centric_move(std::pair<double, double> movement_vector);
+        void field_centric_move(std::pair<double, double> movement_vector);
+    private:
+        Controller master_;
+        Motor front_left_;
+        Motor front_right_;
+        Motor back_left_;
+        Motor back_right_;
+};
---
+++ src/common_code/x_drive.cpp
@@ -0,0 +1,41 @@
+#include "x_drive.h"
+
+
+// add in a super for parent drive class later
+x_drive::x_drive(Controller master, Motor front_left, Motor front_right, Motor back_left, Motor back_right, Imu imu) : 
+    master_(master), front_left_(front_left), front_right_(front_right), back_left_(back_left), back_right_(back_right) {}
+ 
+void x_drive::robot_centric_move(std::pair<double, double> movement_vector){
+    double magnitude = movement_vector.first;
+    double angle = movement_vector.second;
+
+    //need to double check the sqrt 2 thing but this should work
+    double move_x = magnitude * cos(angle) / sqrt(2.0);
+    double move_y = magnitude * cos(angle) / sqrt(2.0);
+
+    
+    double left_move = (move_y - move_x);
+    double right_move = (move_y + move_x);
+    front_left_.move(right_move);
+    front_right_.move(left_move);
+    back_left_.move(left_move);
+    back_right_.move(right_move);
+    
+    
+    
+
+    //b(toward left) = (y-x)/root2 while a(toward right) = (y+x)/root2
+
+}
+
+void x_drive::field_centric_move(std::pair<double, double> movement_vector){
+    const double pi = 3.14159265358979323846; //cant find pi constant in math
+    IMU imu_ = IMU(1); //temp imu thing so I can write code
+
+
+    double radians_from_imu = pi * (360.0 - imu_.get_heading()) / 180.0; //left is 90
+    double adjusted_angle = movement_vector.second - radians_from_imu;
+
+    std::pair<double, double> new_movement_vector(movement_vector.first, adjusted_angle);
+    this->robot_centric_move(new_movement_vector);
+}
