c4dd74840edbaec9e306579832a9646d9131976c
Rishi V. <rishiv5@illinois.edu>
2023-11-03 19:46:41 -0500

    Added strafing PID functions

--- include/common_code/PID.h
+++ include/common_code/PID.h
@@ -2,23 +2,31 @@
 
 class PID{
     public:
-        PID(double kp, double ki, double kd, double turnKp, double turnKi, double turnKd);
+        PID(double Kp, double Ki, double Kd, double turnKp, double turnKi, double turnKd, double strafeKp, double strafeKi, double strafeKd);
+        void setFwdConstants(double kp, double ki, double kd);
+        void setTurnConstants(double kp, double ki, double kd);
+        void setStrafeConstants(double kp, double ki, double kd);
+
         void drivePID(double target);
+        void drivePID(double target, int angle);
+        void drivePID(double target, int angle, double strafeTarget);
+
         void turnPID(int angle);
-        void comboPID(double target, int angle);
-        void setConstants(double kp, double ki, double kd, double turnKp, double turnKi, double turnKd);
+        void strafePID(double strafeTarget);
 
     private:    
         //Fwd PID constants
         double KP;
         double KI;
         double KD;
+
         //Turn PID constants
         double turnKP;
         double turnKI;
         double turnKD;
+
         //Strafe PID constants
-        // double strafeKP;
-        // double strafeKI;
-        // double strafeKD;
+        double strafeKP;
+        double strafeKI;
+        double strafeKD;
 };
--- src/common_code/PID.cpp
+++ src/common_code/PID.cpp
@@ -6,7 +6,7 @@ pros::Rotation yEnc(10);
 pros::Rotation xEnc(14);
 pros::IMU imu(20);
 
-PID::PID(double Kp, double Ki, double Kd, double turnKp, double turnKi, double turnKd){
+PID::PID(double Kp, double Ki, double Kd, double turnKp, double turnKi, double turnKd, double strafeKp, double strafeKi, double strafeKd){
     //FWD PID constants
     KP = Kp;
     KI = Ki;
@@ -15,15 +15,28 @@ PID::PID(double Kp, double Ki, double Kd, double turnKp, double turnKi, double t
     turnKP = turnKp;
     turnKI = turnKi;
     turnKD = turnKd;
+    //Strafe PID constants
+    strafeKP = strafeKp;
+    strafeKI = strafeKi;
+    strafeKD = strafeKd;
 }
 
-void PID::setConstants(double kp, double ki, double kd, double turnKp, double turnKi, double turnKd){
+void PID::setFwdConstants(double kp, double ki, double kd){
     KP = kp;
     KI = ki;
     KD = kd;
-    turnKP = turnKp;
-    turnKI = turnKi;
-    turnKD = turnKd;
+}
+
+void PID::setTurnConstants(double kp, double ki, double kd){
+    turnKP = kp;
+    turnKI = ki;
+    turnKD = kd;
+}
+
+void PID::setStrafeConstants(double kp, double ki, double kd){
+    strafeKP = kp;
+    strafeKI = ki;
+    strafeKD = kd;
 }
 
 void PID::drivePID(double target){
@@ -57,35 +70,7 @@ void PID::drivePID(double target){
     }
 }
 
-void PID::turnPID(int angle){
-    double heading = imu.get_heading();
-    double tError = angle-heading;
-
-    double lastTError = 0;
-    double tDerivative = 0;
-    double tIntegral = 0;
-
-    double turnSpd;
-    while(std::abs(tError) > 1){
-        heading = imu.get_heading(); //IMU Sensor returns current heading in degrees from 0-360
-        
-        tError = angle - heading;
-
-        tIntegral += tError;
-        if(tError=0||heading>tError)
-            tIntegral = 0;
-        else if(tError<=0)
-            tIntegral = 0;
-        
-        tDerivative = tError - lastTError;
-        lastTError = tError;
-
-        turnSpd = turnKP * tError + turnKI * tIntegral + turnKD * tDerivative;   
-        // turn(turnSpd); //TODO: Implement turn function
-    }
-}
-
-void PID::comboPID(double target, int angle){
+void PID::drivePID(double target, int angle){
     //FWD PID
     double error = target;
     yEnc.reset();
@@ -147,8 +132,162 @@ void PID::comboPID(double target, int angle){
         turnSpd = turnKP * tError + turnKI * tIntegral + turnKD * tDerivative;   
 
         ///////////////////////////
-        
         // drive(fwdSpd, turnSpd); //TODO: Implement drive+turn function
     }
 }
 
+void PID::drivePID(double target, int angle, double strafeTarget){
+    //FWD PID
+    double error = target;
+    yEnc.reset();
+    double sensorValue = 0;
+
+    double lastError = 0;
+    double derivative = 0;
+    double integral = 0;
+
+    double fwdSpd;
+
+    //Turn PID
+    double heading = imu.get_heading();
+    double tError = angle-heading;
+
+    double lastTError = 0;
+    double tDerivative = 0;
+    double tIntegral = 0;
+
+    double turnSpd;
+
+    //Strafe PID
+    double sError = strafeTarget;
+    xEnc.reset();
+    double sSensorValue = 0;
+
+    double lastSError = 0;
+    double sDerivative = 0;
+    double sIntegral = 0;
+
+    double strafeSpd;
+    while(std::abs(error) > 1 && std::abs(tError) > 1 && std::abs(sError) > 1){
+
+        ////////////////////////////
+        //        FWD PID         //
+        ////////////////////////////
+        sensorValue = 2*M_PI*(1.96/2)*(yEnc.get_angle()/360); //2*pi*r*(degrees/360)
+        
+        error = target - sensorValue;
+
+        integral += error;
+        //Ensures integral doesn't get too large
+        if(error=0||sensorValue>error)
+            integral = 0;
+        else if(error<=0)
+            integral = 0;
+        
+        //updates derivative and lastError
+        derivative = error - lastError;
+        lastError = error;
+
+        fwdSpd = KP * error + KI * integral + KD * derivative;  
+
+        ////////////////////////////
+        //        Turn PID        //
+        ////////////////////////////
+        heading = imu.get_heading(); //IMU Sensor returns current heading in degrees from 0-360
+        
+        tError = angle - heading;
+
+        tIntegral += tError;
+        if(tError=0||heading>tError)
+            tIntegral = 0;
+        else if(tError<=0)
+            tIntegral = 0;
+        
+        tDerivative = tError - lastTError;
+        lastTError = tError;
+
+        turnSpd = turnKP * tError + turnKI * tIntegral + turnKD * tDerivative;   
+
+        ////////////////////////////
+        //        Strafe PID      //
+        ////////////////////////////
+        sSensorValue = 2*M_PI*(1.96/2)*(xEnc.get_angle()/360); //2*pi*r*(degrees/360)
+        
+        sError = strafeTarget - sSensorValue;
+
+        sIntegral += sError;
+        //Ensures integral doesn't get too large
+        if(sError=0||sSensorValue>error)
+            sIntegral = 0;
+        else if(error<=0)
+            sIntegral = 0;
+        
+        //updates derivative and lastError
+        sDerivative = sError - lastSError;
+        lastSError = sError;
+
+        strafeSpd = strafeKP * sError + strafeKI * sIntegral + strafeKD * sDerivative;   
+        
+        ////////////////////////////
+        // drive(fwdSpd, turnSpd, strafeSpd); //TODO: Implement drive+turn+strafe function
+    }
+}
+
+void PID::turnPID(int angle){
+    double heading = imu.get_heading();
+    double tError = angle-heading;
+
+    double lastTError = 0;
+    double tDerivative = 0;
+    double tIntegral = 0;
+
+    double turnSpd;
+    while(std::abs(tError) > 1){
+        heading = imu.get_heading(); //IMU Sensor returns current heading in degrees from 0-360
+        
+        tError = angle - heading;
+
+        tIntegral += tError;
+        if(tError=0||heading>tError)
+            tIntegral = 0;
+        else if(tError<=0)
+            tIntegral = 0;
+        
+        tDerivative = tError - lastTError;
+        lastTError = tError;
+
+        turnSpd = turnKP * tError + turnKI * tIntegral + turnKD * tDerivative;   
+        // turn(turnSpd); //TODO: Implement turn function
+    }
+}
+
+void PID::strafePID(double strafeTarget){
+    double error = strafeTarget;
+    xEnc.reset();
+    double sensorValue = 0;
+
+    double lastError = 0;
+    double derivative = 0;
+    double integral = 0;
+
+    double strafeSpd;
+    while(std::abs(error) > 1){
+        sensorValue = 2*M_PI*(1.96/2)*(xEnc.get_angle()/360); //2*pi*r*(degrees/360)
+        
+        error = strafeTarget - sensorValue;
+
+        integral += error;
+        //Ensures integral doesn't get too large
+        if(error=0||sensorValue>error)
+            integral = 0;
+        else if(error<=0)
+            integral = 0;
+        
+        //updates derivative and lastError
+        derivative = error - lastError;
+        lastError = error;
+
+        strafeSpd = strafeKP * error + strafeKI * integral + strafeKD * derivative;   
+        // strafe(strafeSpd); //TODO: Implement strafe function
+    }
+}
