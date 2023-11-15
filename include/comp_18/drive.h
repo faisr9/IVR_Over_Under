25c12383789c9eaca26cf477537511f6f33c09f0
ifndef-define <66566910+ifndef-define@users.noreply.github.com>
2023-11-14 23:18:10 -0600

    added comp_18 files and include stuff

--- include/comp_18/drive.h
+++ include/comp_18/drive.h
@@ -1,19 +1,11 @@
 #pragma once
 #include "main.h"
 
-extern int drive_mode_idx;
+class DriveClass : public SubsystemParent
+{
+    private:
 
-void op_drive();
-void toggle_drive_mode();
-std::string get_drive_name();
-
-void tank_drive();
-void arcade_drive();
-void hybrid_drive();
-
-extern std::vector<std::pair<std::string,std::function<void()>>> drive_modes;
-
-// Utility functions
-double normalize_joysticks(double input);
-double sin_scale(double input);
-double square_scale(double input);
+    public:
+        DriveClass(const DriveClass& other) = delete;
+        ~DriveClass();
+};
