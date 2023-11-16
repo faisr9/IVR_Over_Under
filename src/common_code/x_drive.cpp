d017a6ef9d6a9dd4bbf90a6293ea7b95662219f2
Bananapad987 <Bananapad987@users.noreply.github.com>
2023-11-15 19:52:21 -0600

    no constants

--- src/common_code/x_drive.cpp
+++ src/common_code/x_drive.cpp
@@ -6,22 +6,14 @@ x_drive::x_drive(Controller master, Motor front_left, Motor front_right, Motor b
     master_(master), front_left_(front_left), front_right_(front_right), back_left_(back_left), back_right_(back_right), imu_(imu) {}
  
 void x_drive::robot_centric_move(std::pair<double, double> movement_vector, double turn_right_x){
+    // splitting the movement_vector for easy use
     double magnitude = movement_vector.first;
     double angle = movement_vector.second;
-    pros::lcd::set_text(2, std::to_string(magnitude) + "  " + std::to_string(angle));
-    double move_x;
-    double move_y;
-    if (angle == 0){
-        move_x = 0;
-        move_y = 0;
-    }
-    else{
-        //need to double check the sqrt 2 thing but this should work
-        move_x = magnitude * cos(angle);
-        move_y = magnitude * sin(angle);
-    }
 
-    double kmultiplier = .9  / sqrt(2.0);
+    double move_x = magnitude * cos(angle);
+    double move_y = magnitude * sin(angle);
+
+    double kmultiplier = .9;
     double left_move = kmultiplier*(move_y - move_x);
     double right_move = kmultiplier*(move_y + move_x);
     
