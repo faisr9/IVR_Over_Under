4041559d865244333a755279dfc7c82f23e1cf94
Reid Faistl <rfaistl@icloud.com>
2024-01-28 15:01:29 -0600

    changed comments in header file

--- include/comp_18/devices.h
+++ include/comp_18/devices.h
@@ -4,25 +4,25 @@
 // Forward declare all devices here so that all of the robot files and subsystem classes can access them //
 
 // Motors //
-extern pros::Motor right_front_top; // 14
+extern pros::Motor right_front_top;
 extern pros::Motor right_front_bottom;
-extern pros::Motor right_middle; // might actually be port 13
-extern pros::Motor right_back; // might actually be port 12
+extern pros::Motor right_middle;
+extern pros::Motor right_back;
 
-extern pros::Motor left_front_top; // 14
-extern pros::Motor left_front_bottom; // one of 18, 19, or 20
-extern pros::Motor left_middle; // one of 18, 19, or 20
-extern pros::Motor left_back; // one of 18, 19, or 20
+extern pros::Motor left_front_top;
+extern pros::Motor left_front_bottom;
+extern pros::Motor left_middle;
+extern pros::Motor left_back;
 
 extern pros::Motor_Group left_drive_motors;
 extern pros::Motor_Group right_drive_motors;
 
-extern pros::Motor intake; // in = negative, out = positive
+extern pros::Motor intake;
 
 // V5 Sensors //
 extern pros::IMU imu;
-extern pros::Distance distance_sensor;
-extern pros::Rotation radial_rot_sensor;
-extern pros::Rotation transverse_rot_sensor; // reversed so that going right is positive
+extern pros::Distance distance_sensor; // inside the intake/catching funnel; detects if we have a triball
+extern pros::Rotation radial_rot_sensor; // parallel to the normal wheels
+extern pros::Rotation transverse_rot_sensor; // perpendicular to the normal wheels
 
 // Legacy Sensors //
