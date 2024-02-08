cdc4e5d26b1c0f974a4d2e70d0ea639944a5b916
Rishi Vemulapalli <rishivemulapalli@gmail.com>
2024-02-07 23:43:44 -0600

    Fixed comp_18 auton

--- src/comp_18/auton.cpp
+++ src/comp_18/auton.cpp
@@ -4,9 +4,9 @@
 
 void auton18() {
     //1 tile is .61 meters (2 ft)
-	std::vector<std::vector<double>> move1 = {vect(0, 0, 0), vect(3, 0, 0)}; //Movement 1 (Move to intake depot)
-	std::vector<std::vector<double>> move2 = {move1.back(), vect(3.5, 0, .5)}; // Movement 2 (Turn to flick triball)
-    std::vector<std::vector<double>> move3 = {move2.back(), vect(4, 0, 1.5)}; // Movement 3 (Drive forward to score)
+	std::vector<std::vector<double>> move1 = {vect(0, 0), vect(3, 0)}; //Movement 1 (Move to intake depot)
+	std::vector<std::vector<double>> move2 = {move1.back(), vect(3.5, .5)}; // Movement 2 (Turn to flick triball)
+    std::vector<std::vector<double>> move3 = {move2.back(), vect(4, 1.5)}; // Movement 3 (Drive forward to score)
 
 
     // 1. drop intake (activate climbing piston)
@@ -22,7 +22,6 @@ void auton18() {
     pros::delay(100);
 
     pros::lcd::set_text(1, "move 2 start");
-    followPath(move2, tank_drive_18, 300, false);
     move(move2, 300, false, false);
     pros::lcd::set_text(1, "move 2 end");
 
@@ -38,7 +37,7 @@ void auton18() {
     // followPath(move4, tank_drive_18, 180, true);
 }
 
-std::vector<double> vect(double x, double y, double numTiles){
+std::vector<double> vect(double x, double y){
     return {(.61*2.5)+(x*.61), (.61*.5)+(y*.61)};
 }
 
