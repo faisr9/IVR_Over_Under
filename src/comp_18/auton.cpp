651ece37cdebbbf63059bd23917179d4183a67bf
Rishi Vemulapalli <rishivemulapalli@gmail.com>
2024-02-09 16:48:55 -0600

    updated auton 18 in

--- src/comp_18/auton.cpp
+++ src/comp_18/auton.cpp
@@ -6,9 +6,9 @@ void auton18() {
     //1 tile is .61 meters (2 ft)
     std::vector<double> start = vectOff(0, 0); //Start position
     
-	std::vector<std::vector<double>> move1 = {start, vect(3.5, 0.5)}; //Movement 1 (Move to intake depot)
-	std::vector<std::vector<double>> move2 = {move1.back(), vect(4.8, .8)}; // Movement 2 (Turn to flick triball)
-    std::vector<std::vector<double>> move3 = {move2.back(), vect(5.5, 1.3)}; // Movement 3 (Drive forward to score)
+	std::vector<std::vector<double>> move1 = {start, vect(3.8, 0.5)}; //Movement 1 (Move to intake depot)
+	std::vector<std::vector<double>> move2 = {move1.back(), vect(5, .8)}; // Movement 2 (Turn to flick triball)
+    std::vector<std::vector<double>> move3 = {move2.back(), vect(5.2, 1.3)}; // Movement 3 (Drive forward to score)
     std::vector<std::vector<double>> move4 = {move3.back(), vect(3.5, .5)}; // Movement 4 (Drive back to go to pick up ball)
 
     // 1. drop intake (activate climbing piston)
@@ -31,8 +31,7 @@ void auton18() {
     pros::delay(100);
 
     pros::lcd::set_text(1, "move 2 start");
-    move(move2, 90, false, false);
-    turnToAngle(tank_drive_18, 30, 3.0,false, 1.12);
+    move(move2, 45, false, false);
     pros::lcd::set_text(1, "move 2 end");
 
     pros::delay(100);
@@ -45,15 +44,14 @@ void auton18() {
 
     pros::delay(100);
     
-    Intake::getInstance()->set_power(127/1.5);
+    Intake::getInstance()->set_power(127);
 
     pros::delay(100);
 
     pros::lcd::set_text(1, "move 3 start");
-    move(move3, 30, false, false);
-    turnToAngle(tank_drive_18, 5, 3.0,false, 1.12);
+    turnToAngle(tank_drive_18, 30, 3.0,false, 1.12);
+    move(move3, 5, false, false);
     pros::lcd::set_text(1, "move 3 end");
-
     pros::delay(1000);
 
     Intake::getInstance()->set_power(0);
@@ -82,5 +80,5 @@ std::vector<double> vectOff(double x, double y){
 }
 
 void move(std::vector<std::vector<double>> moveVec, int angle, bool isReversed, bool isSpinAtEnd){
-    followPath(moveVec, tank_drive_18, angle, isReversed, false,  false, 0.5, 3.0, 200.0/2, 450.0/2, 40.0/2, false);
+    followPath(moveVec, tank_drive_18, angle, isReversed, false,  false, 0.5, 3.0, 200.0/10, 450.0/10, 40.0/10, false);
 }
