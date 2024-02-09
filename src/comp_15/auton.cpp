195d7ea4e57ab66f75eff9bd238689bf77ca2eed
Reid Faistl <rfaistl@icloud.com>
2024-02-09 17:47:48 -0600

    Ready for shooter testing

--- src/comp_15/auton.cpp
+++ src/comp_15/auton.cpp
@@ -9,22 +9,6 @@ void auton_15() {
     const double kSTARTING_Y = 0.3;
     const double kSTARTING_ANGLE = 60.0;
 
-
-    pros::Task shooting_task{[=] {
-
-        while (1) {
-            pros::delay(1000);
-        }
-
-        // followPath(to_pole, drive, 135, false, false, false, 0.5, 3.0, 200.0, 450.0, 200.0);
-	}}; // lambda function with a task
-
-    const int kMAX_SHOOTING_TIME = 15000;
-    double start_shoot_time = pros::millis();
-    while (pros::millis() < start_shoot_time + kMAX_SHOOTING_TIME && path_task.get_state() != pros::E_TASK_STATE_DELETED) {
-        pros::delay(100);
-    }
-
     drive.getOdom().initTracker(kSTARTING_X, kSTARTING_Y, kSTARTING_ANGLE);
     pros::delay(50);
 
@@ -35,16 +19,48 @@ void auton_15() {
 		}
 	}}; // lambda function with a task
 
-	// do something noteworthy
-
-
-
+    // release intake and catapult
     Intake::getInstance()->set_power(50);
     pros::delay(500);
-
     Intake::getInstance()->set_power(0);
 
-    
+    // start shooting portion of autonomous
+    pros::Task shooting_task{[=] {
+        while (1) {
+            CompetitionCatapult::getInstance()->set_cata_mode("P");
+            // cata down
+            // delay for triball placement
+            pros::delay(3000);
+            // doinker down
+            DoinkerClass::getInstance()->move(DoinkerClass::DOWN);
+            // small delay
+            pros::delay(500);
+            // doinker up
+            DoinkerClass::getInstance()->move(DoinkerClass::UP);
+            // medium delay
+            pros::delay(2000);
+            // cata up
+            CompetitionCatapult::getInstance()->set_cata_mode("RP");
+            // repeat
+
+            pros::delay(1000);
+        }
+	}}; // lambda function with a task
+
+    // create while loop that will exit after kMAX_SHOOTING_TIME/1000 seconds
+    const int kMAX_SHOOTING_TIME = 15000;
+    const double start_shoot_time = pros::millis();
+    // no need to check if task has terminated because it can't
+    while (pros::millis() < start_shoot_time + kMAX_SHOOTING_TIME) {
+        pros::delay(100);
+    }
+    shooting_task.suspend();
+    // reset catapult and doinker to known state
+    CompetitionCatapult::getInstance()->set_cata_mode("R");
+    DoinkerClass::getInstance()->move(DoinkerClass::UP);
+
+
+    // drive robot to pole to touch
     pros::Task path_task{[=] {
         std::vector<std::vector<double>> to_pole = {{kSTARTING_X, kSTARTING_Y}, {1.0, 1.2}, {1.58, 0.66}};
         followPath(to_pole, drive, 135, false, false, false, 0.5, 3.0, 200.0, 450.0, 200.0);
@@ -52,13 +68,14 @@ void auton_15() {
 
     // pros::lcd::set_text(7, "During path state" + std::to_string(path_task.get_state()));
 
+    // another abort bc the robot might not get precisely to the point (and just be stuck driving into the pole)
     const double kPATH_MAX_TIME = 3200;
     double start_time = pros::millis();
     while (pros::millis() < start_time + kPATH_MAX_TIME && path_task.get_state() != pros::E_TASK_STATE_DELETED) {
         pros::delay(100);
     }
 
-    // if the path is still going
+    // if the path is still going cancel it, but if it's finished don't call suspend
     if (path_task.get_state() != pros::E_TASK_STATE_DELETED && path_task.get_state() != pros::E_TASK_STATE_INVALID) {
         // if call suspend on deleted task program will crash! (data abort)
         // this is why program would only crash if program successfully completed
@@ -66,34 +83,9 @@ void auton_15() {
         stopMotors(drive);
     }
 
-    // ready = 1
-    // running = 0
-    // suspended = 3
-    // goes chronlogical
-
-    // crashing if suspending after path successfully finishes
-
-    // pros::lcd::set_text(7, "After path state" + std::to_string(path_task.get_state()));
-    // path_task.remove();
-
-    // uint32_t state = path_task.get_state();
-    // if (state == pros::E_TASK_STATE_RUNNING) {
-    //     pros::lcd::set_text(7, "Path state is running");
-    // } else if (state == pros::E_TASK_STATE_SUSPENDED) {
-    //     pros::lcd::set_text(7, "Path state is suspended");
-    // } else {
-    //     pros::lcd::set_text(7, "Path state is something else");
-    // }
-    // pros::lcd::set_text(6, std::to_string(state));
-    // pros::delay(100);
-
+    // pros::E_TASK_STATE enums go chronologically based on order listed in docs starting at 0
     // OK SO WHAT HAPPENS IS IF THE TASK IS DONE PROS AUTOMATICALLY DELETES IT SO CALLING SUSPEND ON DELETED TASK = CRASH
     // CAN TELL WITH GET_STATE == E_TASK_STATE_DELETED
-
-
-
-
-    pros::lcd::set_text(1, "Starting the path!");
 
 	odom_task.suspend();
     pros::lcd::set_text(1, "Done with the path!");
