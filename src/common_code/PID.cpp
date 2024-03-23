e8790764e61f6f1ad467651229f943f76011ca95
Rishi Vemulapalli <rishivemulapalli@gmail.com>
2024-03-22 19:46:20 -0500

    fresh start

---
+++ src/common_code/PID.cpp
@@ -0,0 +1,73 @@
+#pragma once
+#include "PID.h"
+
+PID::PID(double kp, double ki, double kd, double min, double max){
+    pid_consts.kP = kp;
+    pid_consts.kI = ki;
+    pid_consts.kD = kd;
+}
+
+PID::PID(double kp, double ki, double kd, double min, double max){
+    pid_consts.kP = kp;
+    pid_consts.kI = ki;
+    pid_consts.kD = kd;
+    pid_consts.OUT_MIN = min;
+    pid_consts.OUT_MAX = max;
+}
+
+//Set kP
+void PID::set_kP(double kp){
+    pid_consts.kP = kp;
+}
+
+//Set kI
+void PID::set_kI(double ki){
+    pid_consts.kI = ki;
+}
+
+//Set kD
+void PID::set_kD(double kd){
+    pid_consts.kD = kd;
+}
+
+void PID::setConstants(double kp, double ki, double kd){
+    pid_consts.kP = kp;
+    pid_consts.kI = ki;
+    pid_consts.kD = kd;
+}
+
+PID::PID_state_s PID::getState(){
+    return pid_state;
+}
+
+PID::PID_consts_s PID::getConstants(){
+    return pid_consts;
+}
+
+double PID::updatePID(double target, double current, double tolerance){
+    pid_state.error = target-current;
+    if(std::abs(pid_state.error) > tolerance){        
+        pid_state.error = target - current;
+        pid_state.integral += pid_state.error;
+        //Ensures integral doesn't get too large
+        if((pid_state.error > 0 && pid_state.lastError < 0) || (pid_state.error < 0 && pid_state.lastError > 0))
+            pid_state.integral = 0;
+        //Update derivative and lastError
+        pid_state.derivative = pid_state.error - pid_state.lastError;
+        pid_state.lastError = pid_state.error; 
+        pid_state.output = pid_consts.kP * pid_state.error + pid_consts.kI * pid_state.integral + pid_consts.kD * pid_state.derivative;
+        //Ensures output doesn't exceed max/min (saturation)
+        if(pid_state.output > pid_consts.OUT_MAX){
+            pid_state.output = pid_consts.OUT_MAX;
+            pid_state.saturated = true;
+        } else if(pid_state.output < pid_consts.OUT_MIN){
+            pid_state.output = pid_consts.OUT_MIN;
+            pid_state.saturated = true;
+        } else {
+            pid_state.saturated = false;
+        }
+        return pid_state.output;
+    }
+    pid_state.saturated = true;
+    return 0;
+}
