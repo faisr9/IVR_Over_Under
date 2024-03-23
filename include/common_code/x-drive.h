e09b3088c363f614844963b6873bac9764f64492
zmartin96 <zachary1131@gmail.com>
2024-03-22 19:42:06 -0500

    created new branch to avoid merge conflicts

---
+++ include/common_code/x-drive.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "drive_parent.h"
+#include "api.h"
+#include <cmath>
+
+using namespace pros;
+class x_drive : public DriveParent
+{
+public:
+    x_drive(Controller &master, Motor &front_left, Motor &front_right, Motor &back_left, Motor &back_right, Imu &imu);
+    void robot_centric_move(std::pair<double, double> movement_vector); // These 2 don't have turning to satisfy DriveParent
+    void field_centric_move(std::pair<double, double> movement_vector); // pass into other funcs with turning = 0
+    void turn_with_power(double power);                                 // turn in place (movement_vector = 0)
+    void robot_centric_move(std::pair<double, double> movement_vector, double turn_right_x);
+    void field_centric_move(std::pair<double, double> movement_vector, double turn_right_x);
+
+private:
+    Controller &master_;
+    Motor &front_left_;
+    Motor &front_right_;
+    Motor &back_left_;
+    Motor &back_right_;
+};
