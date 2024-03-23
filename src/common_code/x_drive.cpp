4c4bd142a5bd920997be34ffcd0e00b0aa090189
zmartin96 <zachary1131@gmail.com>
2024-03-22 19:58:11 -0500

    x-drive as a class

--- src/common_code/x_drive.cpp
+++ src/common_code/x_drive.cpp
@@ -5,14 +5,14 @@ x_drive::x_drive(Controller &master, Motor &front_left, Motor &front_right, Moto
 
 void x_drive::robot_centric_move(std::pair<double, double> movement_vector, double turn_right_x)
 {
-    // angle uses radians since I use cos and sin
-    //  0 is the right of the robot and pi is forward
+    // cos and sin (cmath) use radians
+    // 0 is the right of the robot and π is forward
 
-    // splitting the movement_vector for easy use
+    // splitting the movement_vector
     double magnitude = movement_vector.first;
     double angle = movement_vector.second;
 
-    // idea is that magnitude controls speed in
+    // magnitude scales direction
     double move_x = magnitude * cos(angle);
     double move_y = magnitude * sin(angle);
 
@@ -24,9 +24,9 @@ void x_drive::robot_centric_move(std::pair<double, double> movement_vector, doub
     back_left_.move(left_move + turn_right_x);
     back_right_.move(right_move - turn_right_x);
 
-    // Total move amount (the square root of robot x^2 + robot x^y) is 2(root 2) times the initial magnitude
+    // Total move amount √(x²+y²) is 2√2 times the initial magnitude
     // This means a magnitude of 1 and theta of pi (with 0 turn) would cause robot motors to all move by 1 and finding the total x and y components,
-    // you get a y of 2 root 2 and x of 0 (since going forward) which leaves us a total of 2root2.
+    // you get a y = 2√2 and x = 0 (forward) which produces a total of 2√2.
 }
 
 void x_drive::field_centric_move(pair<double, double> movement_vector, double turn_right_x)
@@ -37,7 +37,7 @@ void x_drive::field_centric_move(pair<double, double> movement_vector, double tu
     // adjusts angle based on imu
     double adjusted_angle = movement_vector.second - radians_from_imu;
 
-    std::pair<double, double> new_movement_vector(movement_vector.first, adjusted_angle);
+    pair<double, double> new_movement_vector(movement_vector.first, adjusted_angle);
     this->robot_centric_move(new_movement_vector, turn_right_x);
 }
 
