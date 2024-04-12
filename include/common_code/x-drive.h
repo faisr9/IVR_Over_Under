7040be15b85c603cc3a8633a49941508e1206119
zmartin96 <zachary1131@gmail.com>
2024-04-12 18:02:54 -0500

    modified x-drive class to accomodate 8-motor asterisk drive

--- include/common_code/x-drive.h
+++ include/common_code/x-drive.h
@@ -1,25 +1,96 @@
 #pragma once
 #include "drive_parent.h"
 #include "main.h"
-#include <cmath>
+#include <cmath> // for angle calculations
 
 using namespace pros;
 using namespace std;
+
 class x_drive : public DriveParent
 {
 public:
+    /*
+     * run method to be called in opcontrol
+     * reads controller input and moves the robot
+     * based on the input
+     * @return void
+     */
     void run();
+    /*
+     * constructor with no foward wheels (regular holonomic x-drive)
+     * @param master controller to get input from
+     * @param front_left front left motor
+     * @param front_right front right motor
+     * @param back_left back left motor
+     * @param back_right back right motor
+     * @param imu inertial sensor to get angle from
+     */
     x_drive(Controller &master, Motor &front_left, Motor &front_right, Motor &back_left, Motor &back_right, Imu &imu);
-    void turn_with_power(double power);                                 // turn in place (movement_vector = 0)
+    /*
+     * asterisk drive constructor
+     * (holonomic x-drive modified to include foward wheels for pushing force)
+     * @param master controller to get input from
+     * @param front_left motor of front left wheel
+     * @param front_right motor of front right wheel
+     * @param back_left motor of back left wheel
+     * @param back_right motor of back right wheel
+     * @param straight_left_A first motor for left straight wheel (exclusive to asterisk drive)
+     * @param straight_right_A first motor for right straight wheel (exclusive to asterisk drive)
+     * @param straight_left_B second motor for left straight wheel (exclusive to asterisk drive)
+     * @param straight_right_B second motor for right straight wheel (exclusive to asterisk drive)
+     * @param imu inertial sensor to get angle from
+     */
+    x_drive(Controller &master, Motor &front_left, Motor &front_right, Motor &back_left, Motor &back_right, Motor &straight_left_A_, Motor &straight_right_A, Motor &straight_left_B, Motor &straight_right_B, Imu &imu);
+    /*
+     * turn in place (movement_vector = 0)
+     * calls robot_centric_move with movement_vector = {0.0,0.0}
+     * @param power power to turn with
+     * @return void
+     */
+    void turn_with_power(double power);
+    /*
+     * move in robot centric coordinates
+     * @param movement_vector pair of magnitude and angle
+     * @param turn_right_x turn right x
+     * @return void
+     */
     void robot_centric_move(pair<double, double> movement_vector, double turn_right_x);
+    /*
+     * move in field centric coordinates
+     * calculated offset based on imu angle and modifies
+     * movement_vector, then calls robot_centric_move
+     * with the modified movement_vector
+     * @param movement_vector pair of magnitude and angle
+     * @param turn_right_x turn right x
+     */
     void field_centric_move(pair<double, double> movement_vector, double turn_right_x);
-    void robot_centric_move(pair<double, double> movement_vector) { robot_centric_move(movement_vector, 0);}
-    void field_centric_move(pair<double, double> movement_vector) { field_centric_move(movement_vector, 0);}
+    /*
+     * move in robot centric coordinates
+     * calls robot_centric_move with turn = 0
+     * @param movement_vector pair of magnitude and direction of movement
+     * @return void
+     */
+    void robot_centric_move(pair<double, double> movement_vector) { robot_centric_move(movement_vector, 0); }
+    /*
+     * move in field centric coordinates
+     * calls field_centric_move with turn = 0
+     * @param movement_vector pair of magnitude and direction of movement
+     * @return void
+     */
+    void field_centric_move(pair<double, double> movement_vector) { field_centric_move(movement_vector, 0); }
 
 private:
-    Controller master_;
-    Motor front_left_;
-    Motor front_right_;
-    Motor back_left_;
-    Motor back_right_;
+    Controller master_; // controller to get input from
+
+    // primary motors
+    Motor front_left_;  // front left motor
+    Motor front_right_; // front right motor
+    Motor back_left_;   // back left motor
+    Motor back_right_;  // back right motor
+
+    // forward wheels for asterisk drive
+    Motor straight_left_A_;  // first left straight motor
+    Motor straight_right_A_; // first right straight motor
+    Motor straight_left_B_;  // second left straight motor
+    Motor straight_right_B_; // second right straight motor
 };
--- src/common_code/x_drive.cpp
+++ src/common_code/x_drive.cpp
@@ -1,26 +1,56 @@
 #include "x-drive.h"
 
-// add in a super for parent drive class later
+// constructor with no foward wheels
 x_drive::x_drive(Controller &master, Motor &front_left, Motor &front_right, Motor &back_left, Motor &back_right, Imu &imu) : master_(master), front_left_(front_left), front_right_(front_right), back_left_(back_left), back_right_(back_right), DriveParent(imu, "x_drive") {}
+
+// astrisk drive constructor (x-drive modified to include foward wheels for pushing force)
+x_drive::x_drive(Controller &master, Motor &front_left, Motor &front_right, Motor &back_left, Motor &back_right, Motor &straight_left_A, Motor &straight_right_A, Motor &straight_left_B, Motor &straight_right_B, Imu &imu) : master_(master), front_left_(front_left), front_right_(front_right), back_left_(back_left), back_right_(back_right), straight_left_A_(straight_left_A), straight_right_A_(straight_right_A), straight_left_B_(straight_left_B), straight_right_B_(straight_right_B), DriveParent(imu, "x_drive") {}
 
 void x_drive::robot_centric_move(pair<double, double> movement_vector, double turn)
 {
     auto speed = 0.0;
-    if (movement_vector.first) speed=127.0*movement_vector.first; // normalized speed of movement times max speed
-    else if (turn) speed=127.0; // if only turning, set speed to max
+    if (movement_vector.first)
+        speed = 127.0 * movement_vector.first; // normalized speed of movement times max speed
+    else if (turn)
+        speed = 127.0;                 // if only turning, set speed to max
     auto dir = movement_vector.second; // direction in radians
 
-    dir -= M_PI / 4; // adjust direction by 45˚ to get the diagonal components of movement
-    auto move_1 = -1*cos(dir); // first diagonal component of movement
-    auto move_2 = sin(dir); // second diagonal component of movement
+    dir -= M_PI / 4;             // adjust direction by 45˚ to get the diagonal components of movement
+    auto move_1 = -1 * cos(dir); // first diagonal component of movement
+    auto move_2 = sin(dir);      // second diagonal component of movement
 
-    auto scaling = speed/max(abs(move_1), abs(move_2)); // speed divided by max of x and y
-    auto turn_factor = 1 - abs(turn); // turn factor
+    auto scaling = speed / max(abs(move_1), abs(move_2)); // speed divided by max of x and y
+    auto turn_factor = 1 - abs(turn);                     // turn factor
 
-    front_left_.move((move_1*scaling*turn_factor - speed*turn)); // fl and br use the first diagonal component
-    front_right_.move((move_2*scaling*turn_factor - speed*turn)); // front motors subtract turn
-    back_left_.move((move_2*scaling*turn_factor + speed*turn)); // bl and fr use the second diagonal component
-    back_right_.move(-(move_1*scaling*turn_factor + speed*turn)); // back motors add turn
+    auto fl_move = (move_1 * scaling * turn_factor - speed * turn); // fl and br use the first diagonal component
+    auto fr_move = (move_2 * scaling * turn_factor - speed * turn); // front motors subtract turn
+    auto bl_move = (move_2 * scaling * turn_factor + speed * turn); // bl and fr use the second diagonal component
+    // direction probably needs to be flipped depending on the orientation of the motors
+    auto br_move = -(move_1 * scaling * turn_factor + speed * turn); // back motors add turn
+
+    // move the four primary motors
+    front_left_.move(fl_move);
+    front_right_.move(fr_move);
+    back_left_.move(bl_move);
+    back_right_.move(br_move);
+
+    // if there are forward wheels, then perform asterisk drive
+    if (straight_left_A_.get_port() != -1 && straight_right_A_.get_port() != -1 && straight_left_B_.get_port() != -1 && straight_right_B_.get_port() != -1)
+    {
+        // left separate from calculations to make
+        // debugging easier if the wheels are misaligned
+        auto theta = M_PI / 4; // assuming perfect 45˚ angle
+
+        // will need to make negative if the wheels are facing the opposite way
+        auto sl_move = fl_move * cos(theta); // should be equivalent to dividing by √2
+        auto sr_move = fr_move * cos(theta); // should be equivalent to dividing by √2
+
+        // move forward/straight wheels
+        straight_left_A_.move(sl_move);
+        straight_right_A_.move(sr_move);
+        straight_left_B_.move(sl_move);
+        straight_right_B_.move(sr_move);
+    }
 }
 
 void x_drive::field_centric_move(pair<double, double> movement_vector, double turn_right_x)
@@ -32,27 +62,27 @@ void x_drive::field_centric_move(pair<double, double> movement_vector, double tu
     double adjusted_angle = movement_vector.second - radians_from_imu;
 
     pair<double, double> new_movement_vector(movement_vector.first, adjusted_angle);
-    this->robot_centric_move(new_movement_vector, turn_right_x);
+    robot_centric_move(new_movement_vector, turn_right_x);
 }
 
 void x_drive::turn_with_power(double power)
 {
-    pair<double, double> new_movement_vector(0.0, 0.0);
-    robot_centric_move(new_movement_vector, power);
+    pair<double, double> new_movement_vector(0.0, 0.0); // no movement
+    robot_centric_move(new_movement_vector, power);     // turn in place
 }
 
 void x_drive::run()
 {
-    pair<double, double> target(0.0, 0.0);
-    auto x=master_.get_analog(E_CONTROLLER_ANALOG_LEFT_X); // target x
-    auto y=master_.get_analog(E_CONTROLLER_ANALOG_LEFT_Y); // target y
+    pair<double, double> target(0.0, 0.0);                   // initialize target (magnitude, direction)
+    auto x = master_.get_analog(E_CONTROLLER_ANALOG_LEFT_X); // target x-coordinate
+    auto y = master_.get_analog(E_CONTROLLER_ANALOG_LEFT_Y); // target y-coordinate
 
-    if (x || y ) // if there is any movement
+    if (x || y) // if there is any movement
     {
-        target.first = min(1.0,sqrt(pow(x,2) + pow(y,2)) / 127.0);///sqrt(2)); // normalized speed to move
-        target.second = atan2(y, x); // direction to move in radians
+        target.first = min(1.0, sqrt(pow(x, 2) + pow(y, 2)) / 127.0); /// sqrt(2)); // normalized speed to move
+        target.second = atan2(y, x);                                  // direction to move in radians
     }
-    
-    auto turn = master_.get_analog(E_CONTROLLER_ANALOG_RIGHT_X)/127.0; // normalized turn amount
-    field_centric_move(target, turn); // move the robot
+
+    auto turn = master_.get_analog(E_CONTROLLER_ANALOG_RIGHT_X) / 127.0; // normalized turn amount
+    field_centric_move(target, turn);                                    // move the robot
 }
