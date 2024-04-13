e48dfb4830d1f7cac85c22bd78a5b8e7480c6a98
ddai20 <ddai20@illinois.edu>
2024-04-12 19:59:28 -0500

    c

--- src/skills_catcher/devices.cpp
+++ src/skills_catcher/devices.cpp
@@ -1,1 +1,30 @@
 #include "skills_18/devices.h"
+
+using namespace pros;
+
+/* Asterisk Drive */
+// Diagonal Motors
+Motor front_left;
+Motor front_right;
+Motor back_left;
+Motor back_right;
+
+// Straight Motors
+Motor left_middle_1;
+Motor left_middle_2;
+Motor right_middle_2;
+Motor right_middle_2;
+
+/* Pnuematics */
+const char LEFT_WING;
+const char RIGHT_WING; 
+const char NET; 
+
+/* IMU */
+pros::IMU imu(21);
+
+/* Distance Sensors */
+pros::Distance distance_sensor_1;
+pros::Distance distance_sensor_2;
+pros::Rotation radial_rot_sensor;
+pros::Rotation transverse_rot_sensor;
--- src/skills_thrower/devices.cpp
+++ src/skills_thrower/devices.cpp
@@ -1,1 +1,28 @@
 #include "skills_15/devices.h"
+
+using namespace pros;
+
+/* Asterisk Drive */
+// Diagonal Motors
+Motor front_left;
+Motor front_right;
+Motor back_left;
+Motor back_right;
+
+/* Catapult */
+Motor cata_1;
+Motor cata_2;
+Rotation cata_rot_sensor;
+
+/* Pnuematics */
+const char LEFT_WING;
+const char RIGHT_WING;
+
+/* IMU */
+IMU imu(21);
+
+/* Distance Sensors */
+Distance distance_sensor_1;
+Distance distance_sensor_2;
+Rotation radial_rot_sensor;
+Rotation transverse_rot_sensor;
