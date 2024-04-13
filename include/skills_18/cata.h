a72bca393a30b283d0423b8b3ae191fa3c0d581b
Bananapad987 <Bananapad987@users.noreply.github.com>
2024-04-12 19:38:34 -0500

    stated cata2

--- include/skills_18/cata.h
+++ include/skills_18/cata.h
@@ -15,14 +15,14 @@ class CompetitionCatapult : public SubsystemParent {
 
     private:
         // reference to passed in motor so it doesn't create a new motor object 
-        CompetitionCatapult(pros::MotorGroup& motorgroup, pros::Rotation& rotation_sensor);
+        CompetitionCatapult(pros::MotorGroup& motorgroup, pros::Rotation& rotation_sensor) : motors(motorgroup), rotation_sensor(rotation_sensor) {};
 
         //instance_ is set to nullptr in cpp file
         inline static CompetitionCatapult* instance_ = nullptr;
 
         pros::MotorGroup& motors;
         pros::Rotation& rotation_sensor;
-
+        int max_position_degrees = 12000; //centidegrees, not sure if should be position or angle
         const int cata_voltage = 100;
         // const int launch_voltage = 50;
 };
---
+++ src/skills_thrower/cata.cpp
@@ -0,0 +1,32 @@
+//I dont remember includes
+#include "cata.h"
+
+
+CompetitionCatapult* CompetitionCatapult::instance_ = nullptr;
+
+CompetitionCatapult* CompetitionCatapult::createInstance(pros::MotorGroup& motorgroup, pros::Rotation& rotation_sensor){
+    if (!instance_) {
+        instance_ = new CompetitionCatapult(motorgroup, rotation_sensor);
+    }
+
+    return instance_;
+}
+CompetitionCatapult* CompetitionCatapult::getInstance(){
+    if (instance_ == nullptr) {
+        throw std::runtime_error("Cata: Attempting to getInstance while instance_ is nullptr");
+    }
+
+    return instance_;
+}
+CompetitionCatapult::~CompetitionCatapult(){
+    if (instance_ != nullptr) {
+        delete instance_;
+        instance_ = nullptr;
+    }
+}
+void CompetitionCatapult::stop(){
+    
+}
+void CompetitionCatapult::prime();
+void CompetitionCatapult::cycle();
+void CompetitionCatapult::release();
