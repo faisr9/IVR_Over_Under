6d1e308a0ee93a30adee693f1103de6833fe4782
ifndef-define <66566910+ifndef-define@users.noreply.github.com>
2024-04-15 22:09:34 -0500

    minor edits + set_rpm and set_power made

--- include/skills_18/intake.h
+++ include/skills_18/intake.h
@@ -5,9 +5,9 @@
 
 class Intake : public SubsystemParent{
     private:
-        pros::Motor *intake_motor_; //Why is this a pointer ------------
+        pros::Motor *intake_motor_;
+        static const int default_rpm_;
         int power_;
-        static const int default_power_; //I assume this should be some random value set here? -------------
 
         //Subsystem parent specific
         static Intake* instance_;
@@ -22,18 +22,10 @@ class Intake : public SubsystemParent{
         void set_power(int power);
 
         // Uses the default power, the power set by set_power, or the power passed in
-        void toggle_on(int power = default_power_);
-        void toggle_off();
-
+        void toggle_on(int power = default_rpm_);
 
         //Subsystem parent specific
         static Intake* createInstance(pros::Motor& motor);
         static Intake* getInstance();
         void stop();
 };
-
-
-// These were here before but I didn't know what they were
-
-// extern bool intake_out;
-// extern bool mag_down;
