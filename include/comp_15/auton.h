007ea25bccc8a8e805078733a027d86c463f63ce
Rishi Vemulapalli <rishivemulapalli@gmail.com>
2024-04-16 19:07:27 -0500

    Reformatted pookie auton

--- include/comp_15/auton.h
+++ include/comp_15/auton.h
@@ -1,6 +1,11 @@
 #pragma once
-#include "comp_15/devices.h"
 #include "comp_15/include_list.h"
+#include "comp_15/devices.h"
+
+// void auton15();
+std::vector<double> vect(double x, double y);
+void move(std::vector<std::vector<double>> moveVec, int angle, bool isReversed, bool isSpinAtEnd, double speedfactor=3.0);
+void turn(double angle);
 
 void win_point_auton();
 void non_win_point_auton();
--- src/pookie/auton.cpp
+++ src/pookie/auton.cpp
@@ -1,43 +1,22 @@
 #include "comp_15/auton.h"
 
 // Auton Methods 
-
-// // Converts inputted tile coordinates, x and y, into meters
-// vector<double> vect(double x, double y)
-// {
-// 	return {(x * .61), (y * .61)};
-// }
-
-void turn_comp(double angle)
-{
-	turnToAngle(tank_drive_15, angle, 2.0, false, 1); // p=1.12 // turndegtolerance=3 //time 150
-}
-
-void move_slw_comp(vector<vector<double>> moveVec, int angle, bool isReversed, bool isSpinAtEnd)
-{
-	double speedfactor = 3.87;
-	followPath(moveVec, tank_drive_15, angle, isReversed, isSpinAtEnd, false, 0.5, 3.0, 200.0 / speedfactor, 450.0 / speedfactor, 40.0 / speedfactor, false, 1.12);
-}
-
-// vector<double> vectOff_comp(double x, double y)
-// {
-// 	return {(.61 * 2.2) + (x * .61), (.61 * .5) + (y * .61)};
-// }
-
-void move_comp(vector<vector<double>> moveVec, int angle, bool isReversed, bool isSpinAtEnd)
-{
-	double speedfactor = 2.7;
-	followPath(moveVec, tank_drive_15, angle, isReversed, isSpinAtEnd, false, 0.5, 3.0, 200.0 / speedfactor, 450.0 / speedfactor, 40.0 / speedfactor, false, 1.12);
+void move(vector<vector<double>> moveVec, int angle, bool isReversed, bool isSpinAtEnd, double speedfactor){
+	for (auto& vec : moveVec) {
+		for (auto& val : vec) { convert::inToM(val); }
+	}
+	followPath(moveVec, tank_drive_15, angle, isReversed, isSpinAtEnd, false, 0.5, 3.0, 200.0 / speedfactor, 600.0 / speedfactor, 40.0 / speedfactor, false, 1.12);
 }
 
 // BEGIN AUTON METHODS //
 
+bool ENABLE_ABORT = true;
 const double AUTON_DURATION = 45000 - 600; // 45 seconds minus 500 milliseconds for safety of threads exiting
 void win_point_auton() {
 
-    const double STARTING_X = 18.0;
-    const double STARTING_Y = 18.0;
-    const double STARTING_ANGLE = 315.0; // 45 degrees south of east
+    const double STARTING_X = 0;
+    const double STARTING_Y = 0;
+    const double STARTING_ANGLE = 0.0;
     const double AUTON_START_TIME = pros::millis();
     double AUTON_RUN_TIME = 0;
     bool end_auton = false;
@@ -52,27 +31,43 @@ void win_point_auton() {
             if (AUTON_RUN_TIME >= AUTON_DURATION) {
                 end_auton = true;
             }
+            else if(ctrl_master.get_digital_new_press(BUTTON_B)) {
+                if (ENABLE_ABORT)
+                    end_auton = true;   
+            }
+
             pros::delay(20);
         }
     }};
-    odom_task.set_priority(TASK_PRIORITY_MEDIUM_HIGH);
+    // odom_task.set_priority(TASK_PRIORITY_MEDIUM_HIGH);
 
     // Auton code here
     pros::Task auton_task {[=] {
-
+        
+		// pros::delay(2000);
     }};
-    auton_task.set_priority(TASK_PRIORITY_MEDIUM_HIGH);
-
+    // auton_task.set_priority(TASK_PRIORITY_MEDIUM_HIGH);
+	
     // End of auton
     while(!end_auton) {
         pros::delay(20);
     }
-    odom_task.remove(); // Delete the odom task
-    auton_task.remove(); // Delete the auton task
+    
+    if (odom_task.get_state() == E_TASK_STATE_RUNNING || odom_task.get_state() == E_TASK_STATE_READY || odom_task.get_state() == E_TASK_STATE_SUSPENDED) {
+        odom_task.remove(); // Delete the odom task
+    }
+    if (auton_task.get_state() == E_TASK_STATE_RUNNING || auton_task.get_state() == E_TASK_STATE_READY){
+        auton_task.suspend(); // Delete the auton task
+        auton_task.remove();
+    }
+    // auton_task.remove(); // Delete the auton task
     // Stop everything
     // Drive Stop
+    tank_drive_15.move_with_power(0);
     // Intake Stop
+    Intake::getInstance()->stop();
     // Wings Close
+    Pneumatics::getInstance()->getWings()->off();
 }
 
 void non_win_point_auton() {
@@ -101,7 +96,7 @@ void non_win_point_auton() {
 
     // Auton code here
     pros::Task auton_task {[=] {
-
+		
     }};
     auton_task.set_priority(TASK_PRIORITY_MEDIUM_HIGH);
 
